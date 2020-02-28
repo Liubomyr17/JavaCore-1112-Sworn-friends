e9c9742355aaa0e36ec85c18c3a1f722336c0385
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-28 09:10:41 +0200

    Add files via upload

---
+++ JavaCore 1112 Sworn friends/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+/*
+
+1112 sworn friends
+Change two classes of AppleIPhone and SamsungGalaxyS2.
+Inherit Samsung Galaxy S2 from Apple IPhone.
+Requirements:
+1. The Solution class must have an AppleIPhone class.
+2. AppleIPhone class must be public.
+3. The Solution class must have the SamsungGalaxyS2 class.
+4. SamsungGalaxyS2 class must be public.
+5. SamsungGalaxyS2 class must be inherited from AppleIPhone class.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    public class ApplePhone {
+
+    }
+    public class SamsungGalaxyS2 extends ApplePhone {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
